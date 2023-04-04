5c795e0e667bc18509aec41f465a9c0b32a77c0f
unknown <seamuss.mck@gmail.com>
2023-04-03 18:53:38 -0700

    Did migrations, changed timezone, and added basic views and templates

--- web/selecto/selecto/settings.py
+++ web/selecto/selecto/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Los_Angeles"
 
 USE_I18N = True
 
