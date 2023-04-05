c12a83300d8880bbb859107b41b95cab3640954c
jeffery-do <timithy4569@gmail.com>
2023-04-05 00:44:03 -0700

    remove unneeded namespace declaration

--- web/selecto/selecto/urls.py
+++ web/selecto/selecto/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('homepage.urls', namespace='homepage')),
+    path('', include('homepage.urls')),
     path("admin/", admin.site.urls),
     path('index/', include('products.urls')),
 ]
