765093245b53221f85bfe97b4098cff55a0e4f1f
shane-co <seamuss.mck@gmail.com>
2023-04-06 23:06:45 -0700

    Changed testing

--- web/selecto/tests/test_example.py
+++ web/selecto/tests/test_example.py
@@ -3,6 +3,6 @@ from django.urls import reverse
 
 @pytest.mark.django_db
 def test_example(client):
-    url = reverse("homepage:home")
+    url = reverse("products:home")
     response = client.get(url)
     assert response.status_code == 200
