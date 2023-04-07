f9e0b9946abf7ebc1b28bd226439df9af36c41a6
shane-co <seamuss.mck@gmail.com>
2023-04-06 22:24:34 -0700

    Updated layout, integrated chatgpt api

--- web/selecto/products/views.py
+++ web/selecto/products/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Product, Review
 from random import randint
+import openai
+from key import OPENAI_API_KEY
+
+openai.api_key = OPENAI_API_KEY
 
 # Create your views here.
 
@@ -21,7 +25,6 @@ def home(request):
             r = review_list[ randint(0, len(review_list) - 1) ]
         else:
             r = None
-        print('r is here: ', r)
     template = loader.get_template('products/home.html')
     context = {
         'product' : p,
@@ -34,8 +37,10 @@ def home(request):
 def index(request):
     product_list = Product.objects.all()
     template = loader.get_template('products/index.html')
+    test_string = "TEsting string"
     context = {
         'product_list': product_list,
+        'test' : test_string,
     }
     return render(request, 'products/index.html', context)
 
@@ -51,8 +56,32 @@ def details(request, product_id):
 def review_details(request, product_id, review_id):
     review = Review.objects.get(id = review_id)
     template = loader.get_template('products/review_details.html')
+    summary = ''
+    definition = ''
+    if(request.GET.get('define')):
+        response = openai.Completion.create(
+            model="text-curie-001",
+            prompt="Define: " + str(request.GET.get('mytextbox')),
+            temperature=0.6
+        )
+        definition = response.choices[0].text
+        print(response)
+        print(response.choices)
+    if(request.GET.get('summarize')):
+       response = openai.Completion.create(
+            model="text-curie-001",
+            max_tokens=1024,
+            prompt="Summarize: " + str(review.review_content),
+            temperature=0.6
+        )
+       print("Response : ", response.choices[0].text)
+       print(response)
+       print(response.choices)
+       summary = response.choices[0].text
     context = {
         'review' : review,
+        'definition' : definition,
+        'summary' : summary,
     }
     return render(request, 'products/review_details.html', context)
 
