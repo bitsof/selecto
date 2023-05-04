5c350494e7edb5e82891377b35ae527c1fc81e0d
shane-co <seamuss.mck@gmail.com>
2023-05-04 13:02:53 -0700

    added test for details and review details views

--- web/selecto/tests/test_views.py
+++ web/selecto/tests/test_views.py
@@ -1,6 +1,9 @@
 import pytest
 from django.urls import reverse
 import config
+from dataaccesslayer import make_product
+from products.models import Review
+from django.utils import timezone
 
 @pytest.mark.django_db
 def test_home(client):
@@ -13,4 +16,20 @@ def test_index(client):
     url = reverse("products:index")
     response = client.get(url)
     assert response.status_code == 200
-                          
+
+@pytest.mark.django_db
+def test_details_view(client):
+    product = make_product('Test Product', 'This is a test product.')
+    url = reverse('products:details', args=[product.id])
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['product'] == product
+
+@pytest.mark.django_db
+def test_review_details_view(client):
+    product = make_product('Test Product', 'This is a test product.')
+    review = Review.objects.create(review_related_product=product, review_publish_date=timezone.now(), review_content='This is a test review.')
+    url = reverse('products:review_details', args=[product.id, review.id])
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['review'] == review
