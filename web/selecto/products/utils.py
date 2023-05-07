6524abcb906dad8927aa729f0d18832c3510812d
shane-co <seamuss.mck@gmail.com>
2023-05-07 13:53:06 -0700

    added function to generate page titles

---
+++ web/selecto/products/utils.py
@@ -0,0 +1,32 @@
+def get_page_title(*args):
+    page_title = 'Selecto'
+    if len(args) == 0:
+        return page_title
+    try:
+        match args[0]:
+            case 'index':
+                page_title += ' - Index'
+            
+            case 'details':
+                page_title += ' - ' + args[1]
+
+            case 'review_details':
+                page_title += ' - ' + args[1] + ' - Review'
+            
+            case 'contact_us':
+                page_title += ' - Contact Us'
+            
+            case 'about_us':
+                page_title += ' - About Us'
+            
+            case 'login':
+                page_title += ' - Log In'
+            
+            case 'signup':
+                page_title += ' - Sign Up'
+
+        return page_title
+    
+    except TypeError:
+        print('type error')
+        return page_title
