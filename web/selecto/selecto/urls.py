6f179ee54ba2a4e3af57720ff3fe813328436e0f
shane-co <seamuss.mck@gmail.com>
2023-05-08 13:22:01 -0700

    allows login/logout of restfulapi webpages

--- web/selecto/selecto/urls.py
+++ web/selecto/selecto/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('products.urls')),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
