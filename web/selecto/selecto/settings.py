33f217bb5748ca519244438dd4d3ddc57a7db592
shane-mckenzie <seamuss.mck@gmail.com>
2023-06-01 22:53:09 -0700

    changed settings for serving static files in production (#117)

--- web/selecto/selecto/settings.py
+++ web/selecto/selecto/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "selecto.middleware.CrossOriginOpenerPolicyMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -142,6 +143,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATICFILES_DIRS = [BASE_DIR / "static"]  
+# The URL to use when referring to static files (where they will be served from)
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+# Default setting is "django.contrib.staticfiles.storage.StaticFilesStorage"
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
