e156d9a27df4fe12027b5d1091f9359c46efac35
shane-co <seamuss.mck@gmail.com>
2023-06-03 16:59:33 -0700

    changes init_db to have an optional argument for another fixture

--- web/selecto/products/management/commands/init_db.py
+++ web/selecto/products/management/commands/init_db.py
@@ -5,8 +5,14 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = "Initializes the database by running 'makemigrations', 'migrate', and 'loaddata' commands."
 
+    def add_arguments(self, parser):
+        # Replace the default with your fixture name
+        parser.add_argument('fixture', nargs='?', default='initial_data', help="Specify the fixture to load (default: 'initial_data')")
+
     def handle(self, *args, **options):
+        fixture = options['fixture']
+
         call_command("makemigrations")
         call_command("migrate")
-        call_command("loaddata", "initial_data")  # Replace with your fixture name
+        call_command("loaddata", fixture)  
         self.stdout.write(self.style.SUCCESS("Database successfully initialized."))
