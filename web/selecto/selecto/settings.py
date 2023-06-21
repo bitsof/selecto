f207de3d201df1a289f9b5b6f183c1649da15155
qevyn <khtr.421@gmail.com>
2023-06-20 20:19:05 -0700

    delete auth.urls from settings.py

--- web/selecto/selecto/settings.py
+++ web/selecto/selecto/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.contrib.auth.urls",
 ]
 
 ROOT_URLCONF = "selecto.urls"
